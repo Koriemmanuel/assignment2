363a919fbd7660efe5c91e88f650ca070874db35
Okorie Emmanuel <kvngdave210@gmail.com>
2025-05-25 10:27:23 +0100

    done

---
+++ app.py
@@ -0,0 +1,8 @@
+surname = 'okorie'
+last_name = 'emmanuel'
+age = 35
+gpa = 5.0
+price = 2,000
+is_he_white = False
+is_he_black = True 
+print(f'my suranme is {surname}. My age is {age}. is he white? {is_he_white}. is he black? {is_he_black}')
